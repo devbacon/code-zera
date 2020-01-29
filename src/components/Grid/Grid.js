a56592e0bbfab62734a4b4caf90accb9aa0994f0
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-01-28 20:47:55 -0800

    add grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Grid.css';
+
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default Grid;
