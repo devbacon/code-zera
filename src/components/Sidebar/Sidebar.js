ade4ff14593152c405dd1dd7268173fcb4f65757
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-01-28 20:48:18 -0800

    add sidebar component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Sidebar.css';
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
