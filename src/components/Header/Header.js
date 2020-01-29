dec226e80b2579989b87dfaa62470bee490b1260
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2020-01-28 20:48:07 -0800

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default Header;
